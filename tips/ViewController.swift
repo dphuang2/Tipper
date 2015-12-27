268f0a0514aa99424dc9cd467aec96d57355d49e
BuildTools <unconfigured@null.spigotmc.org>
2015-12-27 00:28:21 -0800

    added optional features

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Do any additional setup after loading the view, typically from a nib.
         
     }
@@ -27,9 +28,29 @@ class ViewController: UIViewController {
     override func viewWillAppear(animated: Bool){
         super.viewWillAppear(animated)
         
+        var tipPercentages = [0.18,0.2,0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+        let billFieldDefault = defaults.valueForKey("billDefault")
+        billField.text = billFieldDefault as? String
+        [billField .becomeFirstResponder()]
         let intValue = defaults.integerForKey("integerDefault")
         defaults.synchronize()
         tipControl.selectedSegmentIndex = intValue
+        var fontSizes = [18,23,28]
+        let fontSize = fontSizes[tipControl.selectedSegmentIndex]
+        let fontSizeFloat = CGFloat(fontSize)
+        tipLabel.font = UIFont(name: tipLabel.font.fontName, size: fontSizeFloat)
     }
     
 
@@ -38,10 +59,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func fontChange(sender: AnyObject) {
+        var fontSizes = [18,23,28]
+        let fontSize = fontSizes[tipControl.selectedSegmentIndex]
+        let fontSizeFloat = CGFloat(fontSize)
+        tipLabel.font = UIFont(name: tipLabel.font.fontName, size: fontSizeFloat)
+    }
     @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        let billDefault = billField.text
+        defaults.setValue(billDefault, forKey: "billDefault")
+        
         var tipPercentages = [0.18,0.2,0.22]
         let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
         
         let billAmount = NSString(string: billField.text!).doubleValue
         let tip = billAmount * tipPercentage
